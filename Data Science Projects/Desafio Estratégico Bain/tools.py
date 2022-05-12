46efbe8c2add29f161970b01044c20da1e6eb2ae
luisresende13 <luisresende13@gmail.com>
2022-05-12 01:05:07 -0300

    Bain Strategic Challenge - 1th phase partial submission included

--- Data Science Projects/Desafio Estratégico Bain/tools.py
+++ Data Science Projects/Desafio Estratégico Bain/tools.py
@@ -1,46 +1,66 @@
-from IPython.display import clear_output as co 
 import pandas as pd
+import os
 import sklearn.preprocessing
-le = sklearn.preprocessing.LabelEncoder
+from IPython.display import clear_output as co 
 
+le = sklearn.preprocessing.LabelEncoder
 target = 'area'
+
 class preprocess:
     
     def __init__(self):
         self.name = 'preprocessing functions class'
-    
-    #### Extract predictive and target variables
-    def get_xy(self, data, target='area', fill_na=0):
-        ## 1. Extraindo variáveis preditivas
-        X = data.drop(target, 1)
-        ## 2. Extraindo Variável Álvo
-        Y = data[target].copy()
-        #### Substituição de Valores Vazios para Variável Alvo
-    #     Y[Y.isna()] = fill_na
-        return X,Y
 
-    #### Label Encode pandas dataframe
-    def label_encode(self, X, base=None):
-        if type(base)==type(None): base = X
-        X_lab = X.copy()
-        for column in X:
-            X_lab[column] = le().fit(base[column]).transform(X[column])
-        return X_lab
+    def extract_series(data, key0='city_code', key1='product', split_category='Others',
+                       split_key='product_type', split_values=['temporary', 'permanent'],
+                       sort_by='year', save=False, path='series/{}.csv'):
 
-    ## Separação das amostras para treinamento e teste
-    def split_ts(self, x, y, col='year', horizon=2):
-    #     periods = x[col].sort_values().iloc[:len(x)-horizon]
-        msk = x[col].isin([42, 43])
-        return [x[msk==False], x[msk], y[msk==False], y[msk]]
+        cities = data[key0].unique()
+        products = data[key1].unique()
+        ts_i = {}
+        for city_code in cities:
+            city_ts = data[data[key0]==city_code]
+            for product in products:
+                prod_ts = city_ts[city_ts[key1]==product].sort_values(sort_by).copy()
+                if len(prod_ts):
+                    if product==split_category:
+                        for prod_type in split_values:
+                            prod_type_ts = prod_ts[prod_ts[split_key]==prod_type].copy()
+                            if len(prod_type_ts):
+                                key = product+'-'+prod_type
+                                ts_i[city_code+'-'+key] = prod_type_ts
+                    else:
+                        key = product
+                        ts_i[city_code+'-'+key] = prod_ts
 
-    
-    ### Train test split of time series by key
-    def custom_tts(self, key):
-        x, y = self.get_xy(series[key], target='area', fill_na=0)
-        x_lab = self.label_encode(x[['year']], base=data)
-        return self.split_ts(x_lab, y, col='year', horizon=2)
-    
-    def clean_series(self, series, train_min=1, drop_start_zeros=True, keep_all_zero=True):
+        #### Salvando séries isoladas
+        if save:
+            for key in ts_i.keys():
+                ts_i[key].to_csv(path.format(key), index=True)
+        return ts_i
+
+    def load_series(path='series/', index_col=0):
+        filenames = os.listdir(path)
+        series, cnt, n_files = {}, 0, len(filenames)
+        freq = range(1, n_files, 10)
+        for filename in filenames:
+            cnt+=1
+            if cnt in freq: print(f'Files loaded: {cnt}/{n_files}'); co(wait=True)
+            series[filename[:-4]] = pd.read_csv(path+filename, index_col=index_col)
+        print(f'Done! Loaded {n_files} files.')
+        return series
+
+    def concat_series(series, le_index=True, save=False, path='data/series.csv'):
+        keys= series.keys()
+        X = pd.concat([series[key].set_index('year')[target] for key in keys], 1).sort_index()
+        X.columns = keys;
+        if le_index:
+            X.index = le().fit(X.index).transform(X.index)
+        if save:
+            X.to_csv(path, index=True)
+        return X
+
+    def clean_series(series, train_min=1, drop_start_zeros=True, keep_all_zero=True, save=False, path='data/series clean/{}.csv'):
         clean_series = {}
         test_empty = []
         excluded_test_index = []
@@ -58,10 +78,10 @@ class preprocess:
         n_non_empty_values = 0
         n_train_zeros = 0
 
-        cnt, freq = 0, range(1, n_total, 10)
-        for key in list(series.keys()):
-            cnt+=1
-            if cnt in freq: print(f'Cleaned series: {cnt}/{n_total}'); co(wait=True)
+        cnt = 0
+        keys = list(series.keys())
+        for key in keys:
+            cnt+=1; print(f'Cleaned series: {cnt}/{n_total}'); co(wait=True)
 
             msk = series[key]['year'].isin(['01/01/2016', '01/01/2017'])
             test = series[key][msk]
@@ -104,8 +124,13 @@ class preprocess:
                             trainset_cut = clean_series[key][train_msk].iloc[isNotZero.index(True):]
                             clean_series[key] = pd.concat([trainset_cut, testset])
                             n_train_zeros+=(sum(train_msk)-len(trainset_cut))
-                            
-                            
+
+        if save:
+            cnt=0
+            for key in clean_series.keys():
+                cnt+=1; print(f'Saved clean series: {cnt}/{len(clean_series)}'); co(wait=True)
+                clean_series[key].to_csv(path.format(key), index=True)
+                
         print('Values Count:')
         print( pd.Series(
             [n_total, n_values_total, n_values_test, n_values_train],
@@ -125,3 +150,85 @@ class preprocess:
             ])
         )
         return clean_series, excluded_test_index
+
+    #### Extract predictive and target variables
+    def get_xy(self, data, target='area', fill_na=0):
+        ## 1. Extraindo variáveis preditivas
+        X = data.drop(target, 1)
+        ## 2. Extraindo Variável Álvo
+        Y = data[target].copy()
+        #### Substituição de Valores Vazios para Variável Alvo
+    #     Y[Y.isna()] = fill_na
+        return X,Y
+
+    #### Label Encode pandas dataframe
+    def label_encode(self, X, base=None):
+        if type(base)==type(None): base = X
+        X_lab = X.copy()
+        for column in X:
+            X_lab[column] = le().fit(base[column]).transform(X[column])
+        return X_lab
+
+    ## Separação das amostras para treinamento e teste
+    def split_ts(self, x, y, col='year', test_values=[42,43]):
+        msk = x[col].isin(test_values)
+        return [x[msk==False], x[msk], y[msk==False], y[msk]]
+
+    
+    ### Train test split of time series by key
+    def custom_tts(self, df, le_base, target='area', x_cols=['year'], index_col='year'):
+        x, y = self.get_xy(df, target, fill_na=0)
+        x_lab = self.label_encode(x[x_cols], base=le_base)
+        return self.split_ts(x_lab, y, col=index_col)
+    
+    #### Extracting test data from series (2016 and 2017)
+    def test_data(self, series, data, keys=None):
+        if keys is None: keys = list(series.keys())
+        Y_e = []
+        for key in keys:
+            x_t, x_e, y_t, y_e = self.custom_tts(series[key], data)
+            Y_e.append(y_e)
+        return pd.concat(Y_e)
+
+class category_index:
+    
+    def __init__(self):
+        self.name='extract dataframe indexes by category'
+        
+    def get_ctgr_combs_indexes(data, prods, col1='product', col2='product_type', sep='-', exclude=['Sorghum', 'Açaí']):
+        prod_indexes = {}
+        for prod in prods:
+            if prod not in exclude:
+                if sep in prod: 
+                    product, prod_type = prod.split(sep)
+                else:
+                    product = prod
+                df = data[data[col1]==product].copy()        
+                if '-' in prod: 
+                    df = df[df[col2]==prod_type]
+                prod_indexes[prod] = df.index.copy()
+        return prod_indexes
+
+    def get_ctgrs_indexes(data, col='product_type', cats=['temporary', 'permanent', 'pasture']):
+        cats_indexes = {}
+        for cat in cats:
+            df = data[data[col]==cat]      
+            cats_indexes[cat] = df.index.copy()
+        return cats_indexes
+    
+    #### Mapping between the series test indexes and the series key
+    def get_key_index_test_map(series, Y_e):    
+        key_test_index_map = {}
+        for key in series.keys():    
+            key_index = series[key].index
+            for entry_index in key_index:
+                if entry_index in Y_e.index:
+                    key_test_index_map[entry_index] = key
+        return key_test_index_map
+    
+    #### Mapping between the series keys and associated product_types
+    def get_key_prodtype_map(series):
+        key_prodtype_map = {}
+        for key in series.keys():
+            key_prodtype_map[key] = series[key].iloc[0]['product_type']
+        return key_prodtype_map
