6483ed84973c9357364317ce44e5490f311c3d39
luisresende13 <luisresende13@gmail.com>
2022-08-27 05:23:32 -0300

    Hackaton COR-Rio IV update

--- Hackaton COR IV - Centro de Operações do RJ/ACELERAÇÃO/Modules/geometric_properties.py
+++ Hackaton COR IV - Centro de Operações do RJ/ACELERAÇÃO/Modules/geometric_properties.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from geopy.distance import geodesic
 
 def centroid(lat, lng):
     x_ = sum(lat) / len(lat)
@@ -7,8 +8,11 @@ def centroid(lat, lng):
 
 def box(lat, lng):
     return lat.min(), lat.max(), lng.min(), lng.max()
-    
-def clusters_centroids(lat, lng, labels, include_box=False, include_center_radius=False):
+
+def distance(p1, p2):
+    return list(map(lambda p1, p2: geodesic(p1, p2).meters, p1.values, p2.values))
+
+def clusters_geometry(lat, lng, labels, include_box=False, include_center_radius=False):
     
     unique_labels = labels.unique()
     centroids, boxes = {}, {}
@@ -16,27 +20,31 @@ def clusters_centroids(lat, lng, labels, include_box=False, include_center_radiu
     for label in unique_labels:
         msk = (labels==label).values
         centroids[label] = centroid(lat[msk], lng[msk])
-        if include_box: boxes[label] = box(lat[msk], lng[msk])
+        boxes[label] = box(lat[msk], lng[msk])
     
     centroids = pd.DataFrame(
         list(map(lambda label: centroids[label], labels)),
-        columns=['lat - centroid', 'lng - centroid'],
+        columns=['lat_centroid', 'lng_centroid'],
         index=labels.index
     )
     
+    boxes = pd.DataFrame(
+        list(map(lambda label: boxes[label], labels)),
+        columns=['lat_min', 'lat_max', 'lng_min', 'lng_max'],
+        index=labels.index
+    )
+
+    radius_lat = (boxes['lat_max'] - boxes['lat_min']) / 2
+    radius_lng = (boxes['lng_max'] - boxes['lng_min']) / 2
+    
     if include_box:
-        boxes = pd.DataFrame(
-            list(map(lambda label: boxes[label], labels)),
-            columns=['lat_min', 'lat_max', 'lng_min', 'lng_max'],
-            index=labels.index
-        )
         centroids = pd.concat([centroids, boxes], 1)
-        if include_center_radius:
-            radius_lat = (boxes['lat_max'] - boxes['lat_min']) / 2
-            radius_lng = (boxes['lng_max'] - boxes['lng_min']) / 2
-            centroids['radius_lat'] = abs(radius_lat)
-            centroids['radius_lng'] = abs(radius_lng)
-            centroids['lat - center'] = boxes['lat_min'] + radius_lat
-            centroids['lng - center'] = boxes['lng_min'] + radius_lng
+        
+    if include_center_radius:
+        centroids['lat_center'] = boxes['lat_min'] + radius_lat
+        centroids['lng_center'] = boxes['lng_min'] + radius_lng
+        centroids['horizontal_perimeter'] = distance(centroids[['lat_min', 'lng_center']], centroids[['lat_max', 'lng_center']])
+        centroids['vertical_perimeter'] = distance(centroids[['lat_center', 'lng_min']], centroids[['lat_center', 'lng_max']])
+        centroids['radius'] = centroids[['horizontal_perimeter', 'vertical_perimeter']].max(1) / 2
             
     return centroids
